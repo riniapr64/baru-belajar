ab21c3baa1d69a70ff125beecea8a299454ebe67
Rini Aprilia <17611063@students.uii.ac.id>
2019-11-22 16:24:45 +0700

    commit yang pertama

---
+++ regression_dev.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+from sklearn.externals import joblib
+import numpy as npapp = Flask(__name__, template_folder='templates')@app.route('/')
+def student():
+   return render_template("home.html")def ValuePredictor(to_predict_list):
+    to_predict = np.array(to_predict_list).reshape(-1,1)
+    loaded_model = joblib.load('model.sav')
+    result = loaded_model.predict(to_predict)
+    return result[0]@app.route('/',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+    to_predict_list = request.form.to_dict()
+    to_predict_list=list(to_predict_list.values())
+    to_predict_list = list(map(float, to_predict_list))
+    result = float(ValuePredictor(to_predict_list))
+    return render_template("home.html",result = result)if __name__ == '__main__':
+   app.run(debug = True, host='10.100.61.200')
